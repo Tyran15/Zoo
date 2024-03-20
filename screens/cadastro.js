e8db67a633ce1d5b311d08f7745766ea8ce7931f
Tyran15 <mh15122006@gmail.com>
2024-03-20 17:29:17 -0300

    adição do axios

--- screens/cadastro.js
+++ screens/cadastro.js
@@ -1,24 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, ImageBackground, Image, TextInput, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { styles } from '../src/style';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Text, ImageBackground, Image, TextInput, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { styles } from "../src/style";
+import axios from "axios";
 
 export default function Cadastro() {
   const navigation = useNavigation();
+  const [formData, setFormData] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+  });
+
+  const handleChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const submitChange = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost/bdetec/userInsert",
+        formData
+      );
+      console.log("Cadastro bem-sucedido:", response.data);
+    } catch (error) {
+      console.error("Ocorreu um erro ao cadastrar:", error);
+    }
+  };
 
   return (
-    <ImageBackground source={require('../assets/splash.jpg')} blurRadius={8} style={styles.container}>
-      <Image source={require('../assets/img/tittle.png')} resizeMode="contain" style={styles.img} />
+    <ImageBackground
+      source={require("../assets/splash.jpg")}
+      blurRadius={8}
+      style={styles.container}
+    >
+      <Image
+        source={require("../assets/img/tittle.png")}
+        resizeMode="contain"
+        style={styles.img}
+      />
       <Text style={styles.text}>Cadastro</Text>
-      <TextInput style={styles.input} placeholder="Nome" />
-      <TextInput style={styles.input} placeholder="Email" />
-      <TextInput style={styles.input} placeholder="Senha" />
-      <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.button}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-        <Text style={styles.textLink}>Já tem uma conta? faça login</Text>
-      </TouchableOpacity>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          value={formData.nome}
+          onChangeText={(text) => handleChange("nome", text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={formData.email}
+          onChangeText={(text) => handleChange("email", text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          value={formData.senha}
+          onChangeText={(text) => handleChange("senha", text)}
+        />
+
+        <TouchableOpacity onPress={submitChange} style={styles.button}>
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.textLink}>Já tem uma conta? Faça login</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </ImageBackground>
   );
